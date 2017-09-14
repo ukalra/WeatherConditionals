dc3c4503a2b7e4c198eef3f27059e4b8071b42a6
ukalra <31013732+ukalra@users.noreply.github.com>
2017-09-13 21:37:30 -0700

    Update WeatherConditionals.java

--- WeatherConditionals.java
+++ WeatherConditionals.java
@@ -1,8 +1,8 @@
 public class WeatherConditionals
 {
-    public static String getWeatherAdvice(int temperature, String description){
+    public static String getWeatherAdvice(int temperature, String description, String description2){
         
-     //   if(description.equals("freezing") && description2.equals("cloudy")){}//example of a complex conditional 
+      if(description.equals("freezing") && description2.equals("cloudy")){}//example of a complex conditional 
         if(description.equals("windy")) return "Too windy or cold! Enjoy watching the weather through the window.";
         else if (description.equals("sunny")) return "It’s safe to go outside, 34 degrees and sunny.";
         else if (description.equals("snow")){
